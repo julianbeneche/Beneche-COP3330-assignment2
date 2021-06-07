94b312e59a006d0455aa2f6068e5fa266a60a87f
julian <Julianbeneche@gmail.com>
2021-06-06 21:08:26 -0400

    Innitial Solution

---
+++ beneche-cop3330-ex01/src/main/java/Ex01/Prob.java
@@ -0,0 +1,19 @@
+package Ex01;
+
+import java.util.Scanner;
+
+public class Prob {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+
+        String outputString = "Hello, " + name + ", nice to meet you!";
+
+        System.out.println(outputString);
+
+    }
+
+}
